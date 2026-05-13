bc3ac6cbb1eccf9fe34756bd5e6222f22b82efc3
AbsariBekti <syahfitriabsaribekti@gmail.com>
2026-05-13 13:32:10 +0700

    benerin

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,30 +9,36 @@ use Illuminate\Support\Facades\Hash;
 class AuthController extends Controller
 {
     public function register(Request $request)
-    {
-        // validasi
+{
+    \Log::info('register called');
+    try {
         $request->validate([
             'nama' => 'required',
             'email' => 'required|email|unique:users,email',
             'password' => 'required|min:6'
         ]);
 
-        // simpan user
-        $user = User::create([
-            'nama' => $request->nama,
-            'email' => $request->email,
-            'password' => Hash::make($request->password)
+       $user = User::create([
+    'nama' => $request->nama,
+    'email' => $request->email,
+    'password' => Hash::make($request->password)
         ]);
 
-        // response
         return response()->json([
             'status' => true,
             'message' => 'Registrasi berhasil',
             'data' => [
-                'id_user' => $user->id_user,
-                'nama' => $user->n=`ama,
-                'email' => $user->email
-            ]
+    'id_user' => $user->id,
+    'nama' => $user->nama,
+    'email' => $user->email
+]
         ], 201);
+    } catch (\Exception $e) {
+        return response()->json([
+            'status' => false,
+            'message' => $e->getMessage()
+        ], 500);
     }
+}
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens; // Punyamu tetap ada
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['nama', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
