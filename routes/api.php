9cf461a8344356e6f0d921314bfee30ba3835d33
wiskas-sys <wsinduadi@gmail.com>
2026-05-13 09:14:11 +0700

    balikin GET /destinasi

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DestinasiController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::apiResource('travel-packages', \App\Http\Controllers\TravelPackageController::class);
+
 Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
 
+Route::get('/destinasi', [DestinasiController::class, 'index']);
